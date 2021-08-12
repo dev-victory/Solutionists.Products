174876bce1b6b5b95f81f8fca9b6e7be3e39e81a
dev-victory <karansethi.official@gmail.com>
2021-08-12 12:28:17 +1200

    WIP: Added angular UI for getting product by ID

--- src/Solutionists.Products.Web/ClientApp/src/app/core/api/product.service.ts
+++ src/Solutionists.Products.Web/ClientApp/src/app/core/api/product.service.ts
@@ -10,10 +10,15 @@ export class ProductService {
 
     constructor(httpClient: HttpClient, @Inject('BASE_URL') baseUrl: string) {
         this.httpClient = httpClient;
-        this.baseUrl = baseUrl;
+        // TODO: revert to baseurl
+        this.baseUrl = 'https://localhost:44347/'; //baseUrl;
     }
 
     public getProducts(): Observable<Product[]> {
         return this.httpClient.get<Product[]>( this.baseUrl + 'products');
+    }
+
+    public getProductById(id: string): Observable<Product> {
+        return this.httpClient.get<Product>(`${this.baseUrl}products/${id}`);
     }
 }
